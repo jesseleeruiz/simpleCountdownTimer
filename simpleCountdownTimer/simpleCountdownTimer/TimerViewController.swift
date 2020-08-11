41484353ce8694ca3238553e09d79652264ff46e
jesseleeruiz <jesseleeruiz@gmail.com>
2020-08-10 14:40:00 -1000

    Added some elements to the storyboard. Created TimerVC.

---
+++ simpleCountdownTimer/simpleCountdownTimer/TimerViewController.swift
@@ -0,0 +1,39 @@
+//
+//  TimerViewController.swift
+//  simpleCountdownTimer
+//
+//  Created by Jesse Ruiz on 7/31/20.
+//  Copyright © 2020 Jesse Ruiz. All rights reserved.
+//
+
+import UIKit
+
+class TimerViewController: UIViewController {
+    
+    // MARK: - Properties
+    
+    
+    // MARK: - Outlets
+
+    // MARK: - Lifecycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+        
+    // MARK: - Actions
+    
+    
+    // MARK: - Methods
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
